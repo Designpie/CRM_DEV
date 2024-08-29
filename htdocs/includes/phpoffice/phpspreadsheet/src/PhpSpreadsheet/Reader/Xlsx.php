73238c516ef0f62fdbc67ab5250707498b2a7451
Designpie <Jolena@dpca.co.za>
2024-08-29 16:15:17 +0200

    dev

--- htdocs/includes/phpoffice/phpspreadsheet/src/PhpSpreadsheet/Reader/Xlsx.php
+++ htdocs/includes/phpoffice/phpspreadsheet/src/PhpSpreadsheet/Reader/Xlsx.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8329e3f3c9993fea023df7d233af8c60aab48168d2137b842ecd0d625131f964
-size 115815
+oid sha256:83b576aeef91fa1d8a16d96104ae39ced26e48318c5673398634b1b54d1f5e91
+size 115822
